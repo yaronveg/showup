6491cf8f20c3df5d179a92000072f10e423923fb
Yaron Veg <yaronveg1@gmail.com>
2021-12-28 12:39:44 +0200

    filters start

--- src/components/Pages/SearchResults.js
+++ src/components/Pages/SearchResults.js
@@ -1,3 +1,5 @@
+import { faAngleDown, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import ResultCard from "../ResultCard/ResultCard";
 
@@ -12,8 +14,31 @@ export default function SearchResults() {
         </div>
         <div className="filters">
           <div className="filter instruments">
-            <button className="filter-btn">Bass (electric)</button>
-            <button className="filter-btn">Bass (electric)</button>
+            <button className="filter-title">
+              <FontAwesomeIcon className="title-icon" icon={faAngleDown} />
+              <span>Instruments</span>
+            </button>
+            <div className="filter-drawer">
+              <button className="filter-btn">Bass (electric)</button>
+              <button className="filter-btn">Guitar</button>
+              <button className="filter-btn">Keynoard</button>
+              <button className="filter-btn">Piano</button>
+              <button className="filter-btn">Violin</button>
+              <button className="filter-btn">Drums</button>
+            </div>
+          </div>
+          <div className="filter genres">
+            <button className="filter-title">
+              <FontAwesomeIcon className="title-icon" icon={faAngleRight} />
+              <span>Genres</span>
+            </button>
+            <div className="filter-samples">
+              <button className="filter-btn">Rock</button>
+              <button className="filter-btn">Alternative</button>
+              <button className="filter-btn">Middle-Eastern</button>
+              <button className="filter-btn">Pop</button>
+              <button className="filter-btn">Folk</button>
+            </div>
           </div>
         </div>
       </div>
