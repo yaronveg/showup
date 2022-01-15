633bc09e84ed181982a4329d2c015ce4375252b1
Yaron Veg <yaronveg1@gmail.com>
2022-01-15 09:33:27 +0200

    signup form start

---
+++ src/components/Pages/SignUp.js
@@ -0,0 +1,8 @@
+export default function SignUp() {
+  return (
+    <div className="SignUp">
+      <TextField type="text"></TextField>
+      <TextField type="email"></TextField>
+    </div>
+  );
+}
