028ef06854e27682ba843c3dff9bb2ceaa790c9a
Yaron Veg <Yaronveg1@gmail.com>
2022-01-15 09:50:50 +0200

    signup form start

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./style.css";
 import Header from "./components/Header/Header";
-import UserProfile from "./components/Pages/UserProfile";
-import SearchResults from "./components/Pages/SearchResults";
+// import UserProfile from "./components/Pages/UserProfile";
+// import SearchResults from "./components/Pages/SearchResults";
+import SignUp from "./components/Pages/SignUp";
 
 function App() {
   return (
     <div className="App">
       <Header />
       {/* <UserProfile /> */}
-      <SearchResults />
+      {/* <SearchResults /> */}
+      <SignUp />
     </div>
   );
 }
--- src/components/Pages/SignUp.js
+++ src/components/Pages/SignUp.js
@@ -1,8 +1,28 @@
+// import * as React from "react";
+// import Box from '@mui/material/Box';
+import TextField from "@mui/material/TextField";
+
 export default function SignUp() {
   return (
     <div className="SignUp">
-      <TextField type="text"></TextField>
-      <TextField type="email"></TextField>
+      <TextField
+        type="text"
+        id="name-first"
+        label="First Name"
+        variant="outlined"
+      ></TextField>
+      <TextField
+        type="text"
+        id="name-last"
+        label="Last Name"
+        variant="outlined"
+      ></TextField>
+      <TextField
+        type="email"
+        id="email"
+        label="Email"
+        variant="outlined"
+      ></TextField>
     </div>
   );
 }
