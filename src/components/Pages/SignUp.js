6f167fbaf7c7d7d40a2bfb97cdef09910dfd29c3
Yaron Veg <Yaronveg1@gmail.com>
2022-01-29 13:22:33 +0200

    signup form control

--- src/components/Pages/SignUp.js
+++ src/components/Pages/SignUp.js
@@ -1,45 +1,66 @@
-// import * as React from "react";
-// import Box from '@mui/material/Box';
-import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleFieldChange = (e) => {
+    switch (e.target.id) {
+      case "name-first":
+        setFirstName(e.target.value);
+        break;
+      case "name-last":
+        setLastName(e.target.value);
+        break;
+      case "email":
+        setEmail(e.target.value);
+        break;
+      default:
+        console.log("field change triggered. field not recognized.");
+    }
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log("FN is: ", firstName, ". LN is: ", lastName, ". @ is: ", email);
+  };
+
   return (
     <div className="SignUp">
       <div className="container">
-        <h1 className="text-primary">
-          SignUp <span>to</span> ShowUp
-        </h1>
-        {/* <TextField
-          type="text"
-          id="name-first"
-          label="First Name"
-          variant="outlined"
-          className="form-field"
-        ></TextField> */}
-        <input
-          type="name"
-          id="name-first"
-          placeholder="First Name"
-          className="form-field"
-        />
-        <input
-          type="name"
-          id="name-last"
-          placeholder="Last Name"
-          className="form-field"
-        />
-        <input
-          type="email"
-          id="email"
-          placeholder="Email"
-          className="form-field"
-        />
-        <div className="form-actions">
-          <a href="#login">Already have an acount?</a>
-          <button type="submit" className="btn btn-primary">
-            SUBMIT
-          </button>
-        </div>
+        <form onSubmit={submit}>
+          <h1 className="text-primary">
+            SignUp <span>to</span> ShowUp
+          </h1>
+          <input
+            type="name"
+            id="name-first"
+            placeholder="First Name"
+            className="form-field"
+            onInput={handleFieldChange}
+          />
+          <input
+            type="name"
+            id="name-last"
+            placeholder="Last Name"
+            className="form-field"
+            onInput={handleFieldChange}
+          />
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            className="form-field"
+            onInput={handleFieldChange}
+          />
+          <div className="form-actions">
+            <a href="#login">Already have an acount?</a>
+            <button type="submit" className="btn btn-primary">
+              SUBMIT
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
