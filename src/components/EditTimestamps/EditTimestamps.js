b1c15a5caf180b6574b0e310046c709ca7594118
Yaron Veg <Yaronveg1@gmail.com>
2022-03-03 22:02:41 +0200

    added basic comment layout for removal and edit. moved to component folder

--- src/components/EditTimestamps/EditTimestamps.js
+++ src/components/EditTimestamps/EditTimestamps.js
@@ -1,6 +1,9 @@
 export default function EditTimestamp({ userId }) {
   return (
     <>
+      {/* removal icon next to existing timestamps - removal dialog */}
+      {/* edit icon next to exsisting timestamps - timestamp dialog */}
+
       {/* plus button to show the type selection */}
       {/* type horizontal button list with icons text on hover or active */}
       {/* date input */}
