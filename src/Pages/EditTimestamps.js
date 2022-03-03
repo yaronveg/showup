5487fb6e19cf2e4ce384712ad52484ba98023494
Yaron Veg <Yaronveg1@gmail.com>
2022-03-03 21:58:41 +0200

    EditTimestamps comment layout

---
+++ src/Pages/EditTimestamps.js
@@ -0,0 +1,14 @@
+export default function EditTimestamp({ userId }) {
+  return (
+    <>
+      {/* plus button to show the type selection */}
+      {/* type horizontal button list with icons text on hover or active */}
+      {/* date input */}
+      {/* subType dropdown if needed */}
+      {/* dynamic structure by type */}
+      {/* // detail text input */}
+      {/* // text text input */}
+      {/* subText text input */}
+    </>
+  );
+}
